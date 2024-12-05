ea6c62a4980b40f57222fddd86bf1334f0848847
Muideen Popoola <popoolamuideen61@gmail.com>
2024-12-05 04:47:58 +0100

    Fixed pug bug

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ const reportRouter = require('./routes/reportRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
